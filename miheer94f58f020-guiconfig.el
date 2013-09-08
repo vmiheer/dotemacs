834df8d46c43a7be49f1e29beae24124c44aad34
miheer vaidya <v.miheer@gmail.com>
2013-09-08 23:55:56 +0530

    Support for babel and trailing spaces

--- miheer94f58f020-guiconfig.el
+++ miheer94f58f020-guiconfig.el
@@ -9,6 +9,7 @@
 (display-time)
 (add-to-list 'load-path "~/.emacs.d/plugins")
 (require 'column-marker)
-(setq show-trailing-whitespace t)
+(setq-default show-trailing-whitespace t)
+(show-paren-mode t)
 (desktop-save-mode 1)
 (provide 'miheer94f58f020-guiconfig)
