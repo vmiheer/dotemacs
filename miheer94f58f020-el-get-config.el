d7680dd8adc88357c6fe0236c9cfe4c6ea7a6b82
Miheer Vaidya <v.miheer@gmail.com>
2021-01-18 17:24:56 -0700

    Add melpa sources, auto build el-get recipes from package.el

--- miheer94f58f020-el-get-config.el
+++ miheer94f58f020-el-get-config.el
@@ -1,6 +1,9 @@
 ;; El get for jedi emacs and others
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (unless (require 'el-get nil 'noerror)
   (with-current-buffer
       (url-retrieve-synchronously
@@ -10,6 +13,8 @@
       (eval-print-last-sexp))))
 
 (el-get 'sync)
+
+(el-get-elpa-build-local-recipes)
 
 (provide 'miheer94f58f020-el-get-config)
 
