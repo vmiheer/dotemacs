3b0afe336dc74213eb4ca97fd6723af654d85c17
Miheer Vaidya <m.vaidya@utah.edu>
2021-01-19 18:45:06 -0800

    Cleanup-guiconfig

--- miheer94f58f020-guiconfig.el
+++ miheer94f58f020-guiconfig.el
@@ -1,16 +1,11 @@
 ;; Stop GUI
+(require 'f)
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
   (when (fboundp mode) (funcall mode -1)))
-;; Pop up required by AC
-(add-to-list 'load-path "~/.emacs.d/el-get/popup")
+
 (if (display-graphic-p)
     (progn
-      (set-default-font "Ubuntu Mono:antialias=subpixel")
-      (set-default-font "Monaco 9")))
-
-(set-face-attribute 'default nil
-		    :height 160
-		    :weight 'normal)
+      (set-frame-font "cascadia mono" nil t)))
 
 (setq display-time-day-and-date t
       display-time-24hr-format t)
@@ -24,52 +19,10 @@
 (show-paren-mode t)
 (desktop-save-mode 1)
 
-(add-to-list 'load-path "~/.emacs.d/plugins/powerline")
+(add-to-list 'load-path (f-join el-get-dir "powerline"))
 (require 'powerline)
 (setq powerline-arrow-shape 'slant)
 (powerline-default-theme)
-
-
-;; gnus
-(copy-face 'font-lock-variable-name-face 'gnus-face-6)
-(setq gnus-face-6 'gnus-face-6)
-(copy-face 'font-lock-constant-face 'gnus-face-7)
-(setq gnus-face-7 'gnus-face-7)
-(copy-face 'gnus-face-7 'gnus-summary-normal-unread)
-(copy-face 'font-lock-constant-face 'gnus-face-8)
-(set-face-foreground 'gnus-face-8 "gray50")
-(setq gnus-face-8 'gnus-face-8)
-(copy-face 'font-lock-constant-face 'gnus-face-9)
-(set-face-foreground 'gnus-face-9 "gray70")
-(setq gnus-face-9 'gnus-face-9)
-(setq gnus-summary-make-false-root 'dummy)
-(setq gnus-summary-make-false-root-always nil)
-(defun oxy-unicode-threads () 
-  (interactive)
-  (setq gnus-summary-dummy-line-format "    %8{│%}   %(%8{│%}                       %7{│%}%) %6{□%}  %S\n"
-	gnus-summary-line-format "%8{%4k│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{│%} %6{%B%} %s\n"
-	gnus-sum-thread-tree-indent " "
-	gnus-sum-thread-tree-root "■ "
-	gnus-sum-thread-tree-false-root "□ "
-	gnus-sum-thread-tree-single-indent "▣ "
-	gnus-sum-thread-tree-leaf-with-other "├─▶ "
-	gnus-sum-thread-tree-vertical "│"
-	gnus-sum-thread-tree-single-leaf "└─▶ "))
-
-(defun oxy-unicode-threads-heavy () 
-  (interactive)
-  (setq gnus-summary-line-format "%8{%4k│%}%9{%U%R%z%}%8{│%}%*%(%-23,23f%)%7{║%} %6{%B%} %s\n"
-	gnus-summary-dummy-line-format "    %8{│%}   %(%8{│%}                       %7{║%}%) %6{┏○%}  %S\n"
-	gnus-sum-thread-tree-indent " "
-	gnus-sum-thread-tree-root "┏● " 
-	gnus-sum-thread-tree-false-root " ○ "
-	gnus-sum-thread-tree-single-indent " ● "
-	gnus-sum-thread-tree-leaf-with-other "┣━━❯ " 
-	gnus-sum-thread-tree-vertical "┃"
-	gnus-sum-thread-tree-single-leaf "┗━━❯ "))
-
-(oxy-unicode-threads-heavy)
-
 
 (windmove-default-keybindings)
 (add-hook 'org-shiftup-final-hook 'windmove-up)
