64b2e44e31edb3088296b2e7318757857ca719e9
Miheer Vaidya <m.vaidya@utah.edu>
2021-02-23 13:44:32 -0700

    Rust config

---
+++ miheer94f58f020-rust-config.el
@@ -0,0 +1,7 @@
+(setq lsp-keymap-prefix "s-l")
+(add-to-list 'load-path "~/.emacs.d/el-get/lsp-mode/")
+(add-to-list 'load-path "~/.emacs.d/el-get/lsp-mode/clients")
+(require 'lsp-mode)
+(require 'lsp-rust)
+(add-hook 'rust-mode-hook #'lsp)
+(provide 'miheer94f58f020-rust-config)
