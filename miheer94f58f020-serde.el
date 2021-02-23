a1b66dc15b2aff6174e5808c8ac890bc94f7d51a
Miheer Vaidya <m.vaidya@utah.edu>
2021-02-23 13:44:10 -0700

    New mode

---
+++ miheer94f58f020-serde.el
@@ -0,0 +1,13 @@
+;; Mode for handling all modes related to serialization/deserialization
+
+(set-face-background 'highlight-indentation-face "#e3e3d3")
+(set-face-background 'highlight-indentation-current-column-face "#c3b3b3")
+
+(dolist (mode '(company-mode highlight-indentation-current-column-mode))
+	(add-hook 'yaml-mode-hook mode))
+
+(add-hook 'yaml-mode-hook
+	  (lambda ()
+	    (outline-minor-mode)
+	    (define-key yaml-mode-map (kbd "TAB") 'outline-toggle-children)
+	        (setq outline-regexp "^ *\\([A-Za-z0-9_-]*: *[>|]?$\\|-\\b\\)")))
