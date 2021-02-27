809ef503bc461760241fd0aaedcac328b6101f0c
Miheer Vaidya <m.vaidya@utah.edu>
2021-02-27 15:18:40 -0700

    add use-package

--- miheer94f58f020-el-get-config.el
+++ miheer94f58f020-el-get-config.el
@@ -37,7 +37,7 @@
   (el-get-elpa-build-local-recipes))
 
 ;; we need to wait for some basic packages before those provided by melpa can be installed
-(setq mv/my-packages '(ccls powerline yasnippet yasnippet-snippets highlight-indentation))
+(setq mv/my-packages '(ccls powerline yasnippet yasnippet-snippets highlight-indentation use-package))
 (el-get 'sync mv/my-packages)
 
 (provide 'miheer94f58f020-el-get-config)
